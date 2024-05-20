bb36f192fd7ae86011e1248e76c549c2b92316c8
PeterAvila24 <avilapeter29@gmail.com>
2024-05-20 15:40:49 -0500

    update (login page)

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React from "react";
 import './App.css';
 import Header from './Header'
 import Home from "./Home";
-import {BrowserRouter as Router , Routes, Route} from "react-router-dom";
+import {BrowserRouter as Router , Routes, Route, useLocation} from "react-router-dom";
 import Checkout from "./Checkout"
+import Login from "./Login";
 
 function App() {
   return (
     //BEM
     <Router>
       <div className="app">
-        <Header/>
+        <ConditionalHeader/>
         <Routes>
-
+          <Route path="/login" element={<Login />}/>
           <Route path="/checkout" element={ <Checkout/> }/>
 
           <Route path="/" element={<Home />}/>
@@ -21,6 +22,12 @@ function App() {
       </div>
     </Router>
   );
+}
+
+function ConditionalHeader(){
+  const location = useLocation();
+  
+  return location.pathname !== '/login' ? <Header/> : null;
 }
 
 export default App;
